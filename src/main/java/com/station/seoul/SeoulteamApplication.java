f341b4678c9e77bb82a1a6f63b19146724900277
hybr1d <hybrid1006@gmail.com>
2023-12-28 10:08:11 +0900

    main commit

---
+++ src/main/java/com/station/seoul/SeoulteamApplication.java
@@ -0,0 +1,13 @@
+package com.station.seoul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeoulteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeoulteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/station/seoul/SeoulteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.station.seoul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeoulteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
